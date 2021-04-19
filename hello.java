5e64ba821dacf2d023d280c3165bbc538f156a97
Dip Singha <dipsingha2@gmail.com>
2021-04-20 02:35:10 +0600

    added display name method.

--- hello.java
+++ hello.java
@@ -2,5 +2,12 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello Java...");
+
+        display("Dip Singha");
     }
+
+    static void display (String name) {
+        System.out.println("I'm : "+name);
+    }
+
 }
